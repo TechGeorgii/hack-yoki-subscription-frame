64ff018fefe8dcc78b5467746fd2924e440cddcc
TechGeorgii <master.wsd064@gmail.com>
2024-03-24 15:55:10 +0300

    subscribe

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -198,12 +198,12 @@ app.transaction("/approve", async (c) => {
 
   // c.frameData?.fid
   return c.contract({
-    abi: ERC20_abi,
+    abi: abi,
     // @ts-ignore
     chainId: chainIdStr,
-    functionName: "approve",
+    functionName: "subscribe",
     //args: [CONTRACT, "6000"],  // we approve 0.006 USDC
-    to: subscriptionTokenAddress,
+    to: CONTRACT,
   });
 });
 
