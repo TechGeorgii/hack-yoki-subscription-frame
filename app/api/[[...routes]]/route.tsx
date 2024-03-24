5cba8a87324d7f8dee9353de6cb9a22b8da18252
TechGeorgii <master.wsd064@gmail.com>
2024-03-24 10:50:40 +0300

    Balance and allowance checks. wallet for now is hardcoded in the config

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -4,9 +4,10 @@ import { Button, Frog, TextInput, parseEther } from "frog";
 import { handle } from "frog/next";
 import { createWalletClient, http, createPublicClient } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { baseSepolia } from "viem/chains";
+import { polygon } from "viem/chains";
 import { PinataFDK } from "pinata-fdk";
 import abi from "./abi.json";
+import ERC20_abi from "./ERC20_abi.json";
 
 const fdk = new PinataFDK({
   pinata_jwt: process.env.PINATA_JWT || "",
@@ -16,47 +17,50 @@ const fdk = new PinataFDK({
 const CONTRACT = process.env.CONTRACT_ADDRESS as `0x` || ""
 
 const account = privateKeyToAccount((process.env.PRIVATE_KEY as `0x`) || "");
+const USDTAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
+const wallet = process.env.WALLET as `0x` || "";  // linked to Farcaster
 
 const publicClient = createPublicClient({
-  chain: baseSepolia,
+  chain: polygon,
   transport: http(process.env.ALCHEMY_URL),
 });
 
 const walletClient = createWalletClient({
   account,
-  chain: baseSepolia,
+  chain: polygon,
   transport: http(process.env.ALCHEMY_URL),
 });
 
-async function checkBalance(address: any) {
-  try {
-    const balance = await publicClient.readContract({
-      address: CONTRACT,
-      abi: abi,
-      functionName: "balanceOf",
-      args: [address, 0],
-    });
-    const readableBalance = Number(balance);
-    return readableBalance;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+async function getUSDTBalance(address: any) {
+  const balance = await publicClient.readContract({
+    address: USDTAddress,
+    abi: ERC20_abi,
+    functionName: "balanceOf",
+    args: [address],
+  });
+  const readableBalance = Number(balance);
+  return readableBalance;
 }
 
-async function remainingSupply() {
-  try {
-    const balance = await publicClient.readContract({
+async function getUSDTAllowance(address: any, spender: string) {
+  const allowance = await publicClient.readContract({
+    address: USDTAddress,
+    abi: ERC20_abi,
+    functionName: "allowance",
+    args: [address, spender],
+  });
+  return Number(allowance);
+}
+
+async function isSubscriber(address : string) : Promise<boolean> {
+    const sub = await publicClient.readContract({
       address: CONTRACT,
       abi: abi,
-      functionName: "totalSupply",
+      functionName: "isSubscriber",
+      args: [address]
     });
-    const readableBalance = Number(balance);
-    return readableBalance;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+    const s = Boolean(sub);
+    return s;
 }
 
 const app = new Frog({
@@ -74,16 +78,16 @@ app.use(
 );
 
 app.frame("/", async (c) => {
-  const balance = await remainingSupply();
-  console.log(balance);
-  if (typeof balance === "number" && balance === 0) {
+  const subscribed = await isSubscriber(wallet);
+  console.log(`${wallet} subscribed: ${subscribed}`);
+  if (subscribed) {
     return c.res({
       image:
         "https://apricot-electoral-bobcat-94.mypinata.cloud/ipfs/QmVbd6q41ZrYsa8iQzVD3vXPNRUejSkQJAZ7AFMi5DGrQP", // thank you
       imageAspectRatio: "1:1",
       intents: [
         <Button.Link href="https://warpcast.com/techgeorgii">
-          Read my content
+          Read me on Warpcast
         </Button.Link>,
       ],
       title: "TechGeorgii - Already subscribed",
@@ -95,15 +99,61 @@ app.frame("/", async (c) => {
         "https://apricot-electoral-bobcat-94.mypinata.cloud/ipfs/Qmd3hQePdBcMuidf991FUFYcvdg5wiKAnpRqjuc1sWRmeZ", // support me
       imageAspectRatio: "1:1",
       intents: [
-        <Button.Transaction target="/buy/0.0005">
-          Buy for 0.005 ETH
-        </Button.Transaction>,
-        <Button action="/ad">Watch ad for 1/2 off</Button>,
+        <Button action="/support">Subscribe for 0.01 USDT/month</Button>,
       ],
       title: "TechGeorgii - Support my work",
     });
   }
 });
+
+app.frame("/support", async (c) => {
+  const bal = await getUSDTBalance(wallet);
+  console.log(`${wallet} USDT balance: ${bal}`)
+  if (bal < 10000)  { // 0.01 USDT 
+    const polygonscanUrl = `https://polygonscan.com/address/${wallet}`;
+
+    return c.res({
+      image: "https://apricot-electoral-bobcat-94.mypinata.cloud/ipfs/QmbJ5C2PRZQM12neTFat35CCU4kMtLqdFPzwxnjGwn81QB", // no USDT
+      imageAspectRatio: "1:1",
+      intents: [
+        <Button.Link href={polygonscanUrl}>
+          Check on Polygonscan
+        </Button.Link>,
+        <Button action="/support">I topped up. Refresh</Button>,
+      ],
+      title: "TechGeorgii - Subscribing",
+    });      
+  }
+
+  const allowance = await getUSDTAllowance(wallet, CONTRACT);
+  if (allowance < 10000) {  // less than 0.01 USDT
+    const approvalsUrl = `https://polygonscan.com/tokenapprovalchecker?search=${wallet}`;
+    return c.res({
+      image: "https://apricot-electoral-bobcat-94.mypinata.cloud/ipfs/Qmdd29NvGKKG3hufRnSWzsPYDod39hhuwqUXmpdd7kvYAs", // no allowance
+      imageAspectRatio: "1:1",
+      intents: [
+        <Button.Link href={approvalsUrl}>
+          View on Polygonscan
+        </Button.Link>,
+        <Button action="/support">I approved. Refresh</Button>,
+      ],
+      title: "TechGeorgii - Subscribing",
+    });      
+  }
+
+  return c.res({
+    action: "/coupon",
+    image:
+      "https://dweb.mypinata.cloud/ipfs/QmeUmBtAMBfwcFRLdoaCVJUNSXeAPzEy3dDGomL32X8HuP",
+    imageAspectRatio: "1:1",
+    intents: [
+      <TextInput placeholder="Wallet Address (not ens)" />,
+      <Button>Receive Coupon</Button>,
+    ],
+    title: "Pinta Hat Store",
+  });
+});
+
 
 app.frame("/finish", (c) => {
   return c.res({
@@ -134,9 +184,9 @@ app.frame("/ad", async (c) => {
 });
 
 app.frame("/coupon", async (c) => {
-  const supply = await remainingSupply();
+  const supply = 0;// await remainingSupply();
   const address = c.inputText;
-  const balance = await checkBalance(address);
+  const balance = 0;// await checkBalance(address);
 
   if (
     typeof balance === "number" &&
